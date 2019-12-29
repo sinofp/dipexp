5c2f85dba67f3e0b08d28891ac629ac2c9c74b36
sinofp <sinofp@tuta.io>
2019-12-29 21:31:52 +0800

    Finish Gaussian filter and Wiener filter
    参考了好多网站：
    http://www.owlnet.rice.edu/~elec539/Projects99/BACH/proj2/wiener.html
    https://medium.com/@bob800530/python-gaussian-filter-%E6%A6%82%E5%BF%B5%E8%88%87%E5%AF%A6%E4%BD%9C-676aac52ea17
    http://dev.theomader.com/gaussian-kernel-calculator/
    http://homepages.inf.ed.ac.uk/rbf/CVonline/LOCAL_COPIES/VELDHUIZEN/node15.html
    https://laurentperrinet.github.io/sciblog/posts/2017-09-20-the-fastest-2d-convolution-in-the-world.html
    https://www.youtube.com/watch?v=l3LMVLbDCic
    
    但最后有很奇怪的网格，也不知道自己做的对不对
    另外听说可能会改题？？？
    
    Squashed commit of the following:
    
    commit 2d1f45220ec677c8207aaaad9a98f10028fab7f4
    Author: sinofp <sinofp@tuta.io>
    Date:   Sun Dec 29 12:09:35 2019 +0800
    
        Add title
    
    commit dbce20de7e29ec12a38dc429ca0e280f2f205c3c
    Author: sinofp <sinofp@tuta.io>
    Date:   Sun Dec 29 11:13:53 2019 +0800
    
        Seperate μ of filter and noise
    
    commit 3f77e2b26a1b7d87d068fcc09bd4a71b458c0f21
    Author: sinofp <sinofp@tuta.io>
    Date:   Sun Dec 29 11:09:08 2019 +0800
    
        整理代码
    
    commit 68d7ecf6239ded70117124f6d0454eb835182f57
    Author: sinofp <sinofp@tuta.io>
    Date:   Sun Dec 29 10:32:49 2019 +0800
    
        Update method generating gaussian_kernel
    
    commit 03f53b66058e56dacb215b577f71c5b2242c8a9d
    Author: sinofp <sinofp@tuta.io>
    Date:   Sat Dec 28 23:33:22 2019 +0800
    
        Upload a patchwork from many resource

---
+++ gaussian&wiener.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.fft import fft2, ifft2
+
+radius = 5
+# μ_filter 必 = 0
+σ_filter = 5
+
+μ_noise = 0
+σ_noise = 1
+
+im = plt.imread("lena512.bmp")
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharex=True, sharey=True)
+ax1.imshow(im, cmap="gray")
+
+ax1.set_title("Original")
+ax2.set_title("Original + Gaussian filter + Gaussian noise")
+ax3.set_title("Original + Gaussian filter + Gaussian noise + Wiener filter")
+
+# 生成高斯核，最后要标准化所以前面×的1/(σ*sqrt(2π))省了
+gaussian_kernel = np.fromfunction(
+    lambda x, y: np.exp(
+        -((x - radius // 2) ** 2 + (y - radius // 2) ** 2) / (2 * σ_filter ** 2)
+    ),
+    (radius, radius),
+)
+gaussian_kernel /= gaussian_kernel.sum()
+
+# 卷积 (转成频域乘法做，快) (但边缘问题呢？)
+newim = np.real(ifft2(fft2(im) * fft2(gaussian_kernel, s=im.shape)))
+
+# 高斯噪声
+noise = np.random.normal(μ_noise, σ_noise, im.shape)
+newim += noise
+newim[newim > 255] = 255
+newim[newim < 0] = 0
+
+ax2.imshow(newim, cmap="gray")
+
+# 维纳滤波
+H = fft2(gaussian_kernel, s=im.shape)
+Y = fft2(newim)
+
+# http://www.owlnet.rice.edu/~elec539/Projects99/BACH/proj2/wiener.html
+# ↓一种比较简单的估计原图功率谱的方法
+Ps = Y * np.conjugate(Y) / newim.shape[0] ** 2
+Pn = σ_noise ** 2  # 噪声功率谱是噪声的方差
+
+K = Pn / Ps
+
+G = np.conjugate(H) / (H ** 2 + K)
+X = G * Y
+reim = np.real(ifft2(X))
+
+ax3.imshow(reim, cmap="gray")
